e75dbdf24a6f0af8f9d41e40606376fba3a70251
Kirill Fomichev <fanatid@ya.ru>
2015-02-02 10:19:27 +0300

    fix tests

--- test/network/switcher.js
+++ test/network/switcher.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 var blockchainjs = require('../../src')
 
 var implementationTest = require('./implementation.js')
@@ -29,10 +31,18 @@ implementationTest({
   describe:       describe,
   description:    'network.Switcher: Two sources (first doesn\'t work), crosscheck = 1',
   getNetworkOpts: function () {
-    var electrumNetwork = new blockchainjs.network.ElectrumJS({
-      url: 'ws://say.hi/',
-      testnet: true
-    })
+    var electrumNetwork = new blockchainjs.network.ElectrumJS({testnet: true})
+    // not connected
+    electrumNetwork.isConnected = function () { return false }
+    // not emit `connect` & `disconnect`
+    electrumNetwork.emit = function (eventName) {
+      if (eventName !== 'connect' && eventName !== 'disconnect') {
+        this.getPrototypeOf().emit.apply(this, _.slice(arguments))
+      }
+    }
+    // not support spv
+    electrumNetwork.supportVerificationMethods = function () { return false }
+
     var chainNetwork = new blockchainjs.network.Chain({testnet: true})
     return [[electrumNetwork, chainNetwork], {crosscheck: 1}]
   }
